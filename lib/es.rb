02b30a9592294c8658ba401bb08adf85431f7e01
Antony Ryabov <anton.ryabov@onetwotrip.com>
2017-06-29 10:19:56 +0300

    Added method for choosing es host and port depend on ENV variables

--- lib/es.rb
+++ lib/es.rb
@@ -29,8 +29,20 @@ class ES
       es.cluster.health
     end
 
+    def es_url
+      if ENV['LITA_ES_HOST'] && ENV['LITA_ES_PORT']
+        "#{ENV['LITA_ES_HOST']}:#{ENV['LITA_ES_PORT']}"
+      elsif ENV['LITA_ES_HOST']
+        return "#{ENV['LITA_ES_HOST']}:9200"
+      elsif ENV['LITA_ES_PORT']
+        return "127.1:#{ENV['LITA_ES_PORT']}"
+      else
+        '127.1:9200'
+      end
+    end
+
     def es
-      @client ||= Elasticsearch::Client.new url: '127.1:9200', log: true
+      @client ||= Elasticsearch::Client.new url: es_url, log: true
     end
   end
 end
